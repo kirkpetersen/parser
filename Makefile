92e399b0fdab49ad4bc3e46591d5bc58c0722c8c
Kirk <kirk@kirk-laptop.(none)>
2009-10-05 17:13:49 -0700

    first commit

---
+++ Makefile
@@ -0,0 +1,7 @@
+# parser makefile
+
+CPPFLAGS = -Wall -Werror -g
+
+all:	parser
+
+parser:	parser.cc
---
+++ parser.cc
@@ -0,0 +1,26 @@
+/*
+ * parser.cc
+ */
+
+#include <assert.h>
+#include <ctype.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include <iostream>
+#include <queue>
+
+using namespace std;
+
+int main(int argc, char * argv[])
+{
+    // simple bottom up parser
+    // read grammar from stdin
+    // very simple tokenizer
+    // upper/lower for terminal/nonterminal
+    // STL for data structures
+    // proof of concept of my dynamic parser idea
+    // - most importantly, can the grammar be modified on the fly?
+
+    return 0;
+}
