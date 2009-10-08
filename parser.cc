40b3888c6cc0cf8eca001b1d08444e8c500b294f
Kirk <kirk@kirk-laptop.(none)>
2009-10-07 17:00:37 -0700

    more code imported from C version

--- parser.cc
+++ parser.cc
@@ -21,32 +21,61 @@ using namespace std;
 
 class parser_item {
 public:
+    string head;
     vector<string> symbols;
     unsigned index;
 
-    parser_item(const list<string> & l);
-};
+    parser_item(const string h, const list<string> & l)
+	: head(h), symbols(l.size()), index(0) {
+	list<string>::const_iterator li;
+	unsigned i = 0;
 
-parser_item::parser_item(const list<string> & l)
-{
-    list<string>::const_iterator li;
-    unsigned i = 0;
+	for(li = l.begin(); li != l.end(); li++) {
+	    symbols[i++] = *li;
+	}
 
-    symbols.resize(l.size());
-
-    for(li = l.begin(); li != l.end(); li++) {
-	symbols[i++] = *li;
+	return;
     }
 
-    index = 0;
+    void dump(void) {
+	cout << head << ": ";
 
-    return;
-}
+	for(unsigned i = 0; i < symbols.size(); i++) {
+	    if(i == index) {
+		cout << "[" << symbols[i] << "]";
+	    } else {
+		cout << "(" << symbols[i] << ")";
+	    }
+	}
+
+	cout << endl;
+    }
+};
 
 class parser_state {
 public:
     list<parser_item> kernel_items;
     list<parser_item> nonkernel_items;
+
+    void dump(void) {
+	list<parser_item>::const_iterator li;
+
+	cout << "kernel items:" << endl;
+
+	for(li = kernel_items.begin(); li != kernel_items.end(); li++) {
+	    parser_item pi(*li);
+
+	    pi.dump();
+	}
+
+	cout << "nonkernel items:" << endl;
+
+	for(li = nonkernel_items.begin(); li != nonkernel_items.end(); li++) {
+	    parser_item pi(*li);
+
+	    pi.dump();
+	}
+    }
 };
 
 class parser {
@@ -61,6 +90,8 @@ public:
     void closure(parser_state & ps);
     void dump(void);
     void read(void);
+
+    string token(void);
 };
 
 void parser::run(void)
@@ -80,12 +111,73 @@ void parser::run(void)
     list<list<string> >::const_iterator li;
 
     for(li = start.begin(); li != start.end(); li++) {
-	parser_item pi(*li);
+	parser_item pi(string("start"), *li);
 
 	ps.kernel_items.push_back(pi);
     }
 
     closure(ps);
+
+    state_stack.push_back(ps);
+
+    unsigned loop = 0;
+
+    int reduced = 0;
+
+    // TODO need to work on tokenizer
+
+    for(unsigned foo = 0; foo < 0; foo++) {
+	cout << "LOOP: " << loop++;
+
+	// First check for reductions
+	list<parser_item>::const_iterator ki;
+
+	for(ki = ps.kernel_items.begin(); ki != ps.kernel_items.end(); ki++) {
+	    parser_item pi(*ki);
+
+	    if(pi.index != pi.symbols.size()) {
+		continue;
+	    }
+
+	    reduced = 1;
+
+	    string head = pi.head;
+	    unsigned ns = pi.symbols.size();
+
+	    cout << "reduce by %s -> " << head;
+
+	    for(unsigned i = 0; i < ns; i++) {
+		// Pop symbols off the stack and print
+		cout << symbol_stack.back();
+
+		symbol_stack.pop_back();
+
+		if(i < ns - 1) {
+		    cout << " ";
+		}
+
+		// Pop this state off the stack
+		ps = state_stack.back();
+		state_stack.pop_back();
+	    }
+
+	    cout << endl;
+
+	    cout << "new symbol on stack: %s" << head;
+
+	    symbol_stack.push_back(head);
+
+	    break;
+	}
+
+	if(reduced) {
+	    continue;
+	}
+
+	cout << "shifting" << endl;
+
+	// check shift on kernel and nonkernel items
+    }
 
     // Create initial kernel item ("start") and push it onto the state stack
 
@@ -114,7 +206,7 @@ void parser::closure(parser_state & ps)
     list<parser_item>::const_iterator ki;
 
     for(ki = ps.kernel_items.begin(); ki != ps.kernel_items.end(); ki++) {
-	parser_item i = *ki;
+	parser_item i(*ki);
 
 	// If this is the end of the item, nothing to do
 	if(i.index == i.symbols.size()) {
@@ -137,7 +229,7 @@ void parser::closure(parser_state & ps)
 	// For each right side of each production, add a non-kernel item
 	for(li = productions[head].begin();
 	    li != productions[head].end(); li++) {
-	    parser_item pi(*li);
+	    parser_item pi(head, *li);
 
 	    ps.nonkernel_items.push_back(pi);
 	}
@@ -151,7 +243,33 @@ void parser::closure(parser_state & ps)
 	// set to 0 and increment for each new item
 	x = 0;
 
-	// for each nonkernel item...
+	for(ki = ps.nonkernel_items.begin();
+	    ki != ps.nonkernel_items.end(); ki++) {
+	    parser_item i(*ki);
+
+	    if(islower(i.symbols[i.index][0])) {
+		continue;
+	    }
+
+	    string head = i.symbols[i.index];
+
+	    if(added[head]) {
+		continue;
+	    }
+
+	    list<list<string> >::const_iterator li;
+
+	    for(li = productions[head].begin();
+		li != productions[head].end(); li++) {
+		parser_item pi(head, *li);
+
+		ps.nonkernel_items.push_back(pi);
+	    }
+
+	    added[head] = true;
+
+	    x++;
+	}
 
     } while(x > 0);
 
@@ -185,6 +303,8 @@ void parser::dump(void)
 {
     map<string, list<list<string> > >::const_iterator mi;
 
+    cout << "* productions:" << endl;
+
     // For each production...
     for(mi = productions.begin(); mi != productions.end(); mi++) {
 	string h = mi->first;
@@ -211,7 +331,38 @@ void parser::dump(void)
 	}
     }
 
+    cout << "* parser state:" << endl;
+
+    cout << "symbol stack:" << endl;
+
+    list<string>::const_iterator sy;
+
+    for(sy = symbol_stack.begin(); sy != symbol_stack.end(); sy++) {
+	cout << *sy << endl;
+    }
+
+    cout << "state stack:" << endl;
+
+    list<parser_state>::const_iterator st;
+
+    for(st = state_stack.begin(); st != state_stack.end(); st++) {
+	parser_state ps = *st;
+
+	ps.dump();
+    }
+
     return;
+}
+
+string parser::token(void)
+{
+    // Simply return the next token
+
+    string s;
+
+    cin >> s;
+
+    return s;
 }
 
 int main(int argc, char * argv[])
@@ -219,9 +370,16 @@ int main(int argc, char * argv[])
     parser p;
 
     p.read();
+
+    cout << "[dumping after read]" << endl;
+
     p.dump();
 
     p.run();
+
+    cout << "[dumping after run]" << endl;
+
+    p.dump();
 
     return 0;
 }
